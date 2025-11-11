bbe3a54e7b9b5313f89daf85f3892b6ccefdc415
Marius Kalvø <mariuskalvo30@gmail.com>
2025-11-11 09:20:40 +0100

    Added code to be deployed

---
+++ code/src/main.js
@@ -0,0 +1,14 @@
+import { getSecondsLeftOfYear, getTimeString } from "./clock";
+import './style.css'
+
+const timeContainer = document.getElementById("time");
+const timeLeft = document.getElementById("seconds-left");
+
+const render = () => {
+  const now = new Date();
+  timeLeft.innerText = getSecondsLeftOfYear(now);
+  timeContainer.innerText = getTimeString(now);
+}
+
+render();
+setInterval(render, 1000);
